da2894ccc6fb5309c5c1f66a71b93e9521fc4a3d
restrellado <rianestrellado3@gmail.com>
2026-02-23 13:32:56 +0800

    model layer

---
+++ backend/src/main/java/com/capstone/desk_nova/config/SpringSecurityConfig.java
@@ -0,0 +1,36 @@
+package com.capstone.desk_nova.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpringSecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                // Disable CSRF (important for POST from JS)
+                .csrf(csrf -> csrf.disable())
+
+                // Enable CORS
+                .cors(cors -> {})
+
+                // Allow all requests
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/**").permitAll()
+                        .anyRequest().permitAll()
+                );
+
+        return http.build();
+    }
+}
---
+++ backend/src/main/java/com/capstone/desk_nova/model/Users.java
@@ -0,0 +1,44 @@
+package com.capstone.desk_nova.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name ="users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "userId")
+    private Long userId;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private String role; //Type will be updated to ROLE
+
+    @Column(nullable = false)
+    private LocalDate createdAt;
+
+    private LocalDate updatedAt;
+
+}
+
+
