9e34088cc9398f285d68b555a271ca51489ece07
restrellado <rianestrellado3@gmail.com>
2026-02-23 15:04:35 +0800

    ticket and ticketcomment object added crud

--- backend/src/main/java/com/capstone/desk_nova/controller/TicketCommentsController.java
+++ backend/src/main/java/com/capstone/desk_nova/controller/TicketCommentsController.java
@@ -1,9 +1,11 @@
 package com.capstone.desk_nova.controller;
 
+import com.capstone.desk_nova.model.TicketComments;
 import com.capstone.desk_nova.service.TicketCommentsService;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/comment")
@@ -12,4 +14,19 @@ public class TicketCommentsController {
 
     private TicketCommentsService ticketCommentsService;
 
+    @GetMapping("/{ticketId}")
+    public ResponseEntity<List<TicketComments>> getCommentsByTicket(@PathVariable Long ticketId) {
+        return ResponseEntity.ok(ticketCommentsService.getCommentsByTicket(ticketId));
+    }
+
+    @PostMapping
+    public ResponseEntity<TicketComments> addComment(@RequestBody TicketComments comment) {
+        return ResponseEntity.ok(ticketCommentsService.addComment(comment));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteComment(@PathVariable Long id) {
+        ticketCommentsService.deleteComment(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- backend/src/main/java/com/capstone/desk_nova/controller/TicketsController.java
+++ backend/src/main/java/com/capstone/desk_nova/controller/TicketsController.java
@@ -1,9 +1,11 @@
 package com.capstone.desk_nova.controller;
 
+import com.capstone.desk_nova.model.Tickets;
 import com.capstone.desk_nova.service.TicketsService;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/ticket")
@@ -11,4 +13,30 @@ import org.springframework.web.bind.annotation.RestController;
 public class TicketsController {
 
     private TicketsService ticketsService;
+
+    @GetMapping
+    public List<Tickets> getAllTickets() {
+        return ticketsService.getAllTickets();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Tickets> getTicketById(@PathVariable Long id) {
+        return ResponseEntity.ok((Tickets) ticketsService.getTicketsById(id));
+    }
+
+    @PostMapping
+    public Tickets createTicket(@RequestBody Tickets ticket) {
+        return ticketsService.createTicket(ticket);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Tickets> updateTicket(@PathVariable Long id, @RequestBody Tickets ticketDetails) {
+        return ResponseEntity.ok(ticketsService.updateTicket(id, ticketDetails));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTicket(@PathVariable Long id) {
+        ticketsService.deleteTicket(id);
+        return ResponseEntity.noContent().build();
+    }
 }
