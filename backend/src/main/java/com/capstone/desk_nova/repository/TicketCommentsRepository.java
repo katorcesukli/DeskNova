0de2df55880bfaf99ba22f0679c086e6bd987b1f
restrellado <rianestrellado3@gmail.com>
2026-02-23 13:40:12 +0800

    repository, base

---
+++ backend/src/main/java/com/capstone/desk_nova/repository/TicketCommentsRepository.java
@@ -0,0 +1,15 @@
+package com.capstone.desk_nova.repository;
+
+import com.capstone.desk_nova.model.TicketComments;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface TicketCommentsRepository extends JpaRepository<TicketComments, Long>{
+
+
+}
