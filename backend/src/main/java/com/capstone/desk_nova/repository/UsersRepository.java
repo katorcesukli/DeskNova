ba5639a884bf4f83c4ac6bc360933481c23d2774
jldavid019 <jldavid019@gmail.com>
2026-02-24 16:04:45 +0800

    misc: remove unused methods

--- backend/src/main/java/com/capstone/desk_nova/repository/UsersRepository.java
+++ backend/src/main/java/com/capstone/desk_nova/repository/UsersRepository.java
@@ -1,9 +1,7 @@
 package com.capstone.desk_nova.repository;
 
-import com.capstone.desk_nova.model.TicketComments;
 import com.capstone.desk_nova.model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,7 +10,6 @@ import java.util.Optional;
 @Repository
 public interface UsersRepository extends JpaRepository<Users, Long>{
 
-    Optional<Users> findById(Long id);
     Optional<Users> findByEmail(String email);
     boolean existsByEmail(String email);
 }
