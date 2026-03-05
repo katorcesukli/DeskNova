486f174371d155213c5f8fbdf5a7f1eb7ba9be85
restrellado <rianestrellado3@gmail.com>
2026-03-05 14:58:50 +0800

    (dto): updated TicketMetric

--- backend/src/main/java/com/capstone/desk_nova/dto/ticket/TicketMetric.java
+++ backend/src/main/java/com/capstone/desk_nova/dto/ticket/TicketMetric.java
@@ -1,12 +1,5 @@
 package com.capstone.desk_nova.dto.ticket;
 
-import com.capstone.desk_nova.dto.person.PersonResponse;
-import jakarta.annotation.Nullable;
-import jakarta.validation.constraints.Null;
-import lombok.Data;
-
-import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Map;
 
 public record TicketMetric(
